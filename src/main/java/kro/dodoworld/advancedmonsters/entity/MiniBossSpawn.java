19fd98d20dbff4810c186a29de2fb219534d3c56
dodoman8067 <dodoman8067@gmail.com>
2023-03-06 22:06:33 +0900

    Add Bombie to MiniBossSpawn.java

--- src/main/java/kro/dodoworld/advancedmonsters/entity/MiniBossSpawn.java
+++ src/main/java/kro/dodoworld/advancedmonsters/entity/MiniBossSpawn.java
@@ -1,11 +1,13 @@
 package kro.dodoworld.advancedmonsters.entity;
 
 import kro.dodoworld.advancedmonsters.AdvancedMonsters;
+import kro.dodoworld.advancedmonsters.entity.miniboss.Bombie;
 import kro.dodoworld.advancedmonsters.entity.miniboss.DiamondZombie;
 import kro.dodoworld.advancedmonsters.entity.miniboss.Inferno;
 import kro.dodoworld.advancedmonsters.entity.miniboss.LeapingSpider;
 import kro.dodoworld.advancedmonsters.entity.miniboss.VoidGloom;
 import org.bukkit.Difficulty;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Monster;
 import org.bukkit.event.EventHandler;
@@ -23,13 +25,20 @@ public class MiniBossSpawn implements Listener {
     public void onSpawn(EntitySpawnEvent event){
         if(event.getEntity().getWorld().getDifficulty().equals(Difficulty.PEACEFUL)) return;
         if(!(event.getEntity() instanceof Monster)) return;
+        if(!isMiniboss(event.getEntity())) return;
         final ThreadLocalRandom rnd = ThreadLocalRandom.current();
         Monster entity = (Monster) event.getEntity();
         if((rnd.nextInt(0, 51) == 1)){
 
             if(entity.getType().equals(EntityType.ZOMBIE)){
+                int bossType = rnd.nextInt(0, 2);
                 event.setCancelled(true);
-                DiamondZombie.createZombie(entity.getLocation());
+                if(bossType == 0){
+                    DiamondZombie.createZombie(entity.getLocation());
+                }
+                if(bossType == 1){
+                    Bombie.createBombie(entity.getLocation());
+                }
             }
             if(entity.getType().equals(EntityType.SPIDER)){
                 event.setCancelled(true);
@@ -45,5 +54,14 @@ public class MiniBossSpawn implements Listener {
                 Inferno.createInferno(event.getLocation());
             }
         }
+    }
+
+    private boolean isMiniboss(Entity entity){
+        for(String s : entity.getScoreboardTags()){
+            if(s.startsWith("adm_miniboss")){
+                return true;
+            }
+        }
+        return false;
     }
 }
