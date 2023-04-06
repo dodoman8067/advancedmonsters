49afa5eb4b263f4f7af22c1998728f43027ca3b0
dodoman8067 <dodoman8067@gmail.com>
2023-04-06 15:30:09 +0900

    Add GoalKey

--- src/main/java/kro/dodoworld/advancedmonsters/entity/boss/ai/NecromancerAttackGoal.java
+++ src/main/java/kro/dodoworld/advancedmonsters/entity/boss/ai/NecromancerAttackGoal.java
@@ -3,7 +3,13 @@ package kro.dodoworld.advancedmonsters.entity.boss.ai;
 import com.destroystokyo.paper.entity.ai.Goal;
 import com.destroystokyo.paper.entity.ai.GoalKey;
 import com.destroystokyo.paper.entity.ai.GoalType;
+import kro.dodoworld.advancedmonsters.AdvancedMonsters;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Creature;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.EnumSet;
@@ -11,8 +17,9 @@ import java.util.EnumSet;
 /**
  * To be added
  */
-public class NecromancerAttackGoal implements Goal<Creature> {
-
+public class NecromancerAttackGoal implements Goal<Creature>, Listener {
+    private final Plugin plugin = AdvancedMonsters.getPlugin(AdvancedMonsters.class);
+    private final GoalKey<Creature> key = GoalKey.of(Creature.class, new NamespacedKey(AdvancedMonsters.getPlugin(AdvancedMonsters.class), "necromancer_attack"));
     private final Creature entity;
 
     public NecromancerAttackGoal(Creature entity){
@@ -21,7 +28,7 @@ public class NecromancerAttackGoal implements Goal<Creature> {
 
     @Override
     public boolean shouldActivate() {
-        return false;
+        return true;
     }
 
     @Override
@@ -32,11 +39,17 @@ public class NecromancerAttackGoal implements Goal<Creature> {
     @Override
     public void start() {
         Goal.super.start();
+        Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    public Creature getEntity() {
+        return entity;
     }
 
     @Override
     public void stop() {
         Goal.super.stop();
+        HandlerList.unregisterAll(this);
     }
 
     @Override
@@ -46,11 +59,11 @@ public class NecromancerAttackGoal implements Goal<Creature> {
 
     @Override
     public @NotNull GoalKey<Creature> getKey() {
-        return null;
+        return key;
     }
 
     @Override
     public @NotNull EnumSet<GoalType> getTypes() {
-        return null;
+        return EnumSet.of(GoalType.TARGET);
     }
 }
