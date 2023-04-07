74a9b64e352e826bda1dfada4137f411dddf7e03
dodoman8067 <dodoman8067@gmail.com>
2023-04-07 17:29:22 +0900

    Comment WorldMonsterLevelIncreaseEvent.java

--- src/main/java/kro/dodoworld/advancedmonsters/event/WorldMonsterLevelIncreaseEvent.java
+++ src/main/java/kro/dodoworld/advancedmonsters/event/WorldMonsterLevelIncreaseEvent.java
@@ -8,43 +8,65 @@ import org.jetbrains.annotations.NotNull;
 
 public class WorldMonsterLevelIncreaseEvent extends WorldEvent implements Cancellable {
 
-
     private final HandlerList handlerList = new HandlerList();
     private final World world;
     private double amount;
     private boolean cancel = false;
 
-
+    /**
+     * Called when monster level is increased
+     * @param world Increased world
+     * @param amount Increased amount
+     */
     public WorldMonsterLevelIncreaseEvent(World world, double amount){
         super(world);
         this.world = world;
         this.amount = amount;
     }
 
+    /**
+     * @return Increased world
+     */
     @NotNull
     @Override
     public World getWorld() {
         return world;
     }
 
+    /**
+     * @return Increased amount
+     */
     public double getAmount() {
         return amount;
     }
 
+    /**
+     * Sets the increased amount
+     * @param amount amount to set
+     */
     public void setAmount(double amount) {
         this.amount = amount;
     }
 
+    /**
+     * @return HandlerList instance
+     */
     @Override
     public @NotNull HandlerList getHandlers() {
         return handlerList;
     }
 
+    /**
+     * @return Returns true when cancelled, otherwise false
+     */
     @Override
     public boolean isCancelled() {
         return cancel;
     }
 
+    /**
+     * @param cancel true if you wish to cancel this event
+     */
     @Override
     public void setCancelled(boolean cancel) {
         this.cancel = cancel;
