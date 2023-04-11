b337f5299727e83e71fe445f0fcc2d4a84bde660
dodoman8067 <dodoman8067@gmail.com>
2023-04-11 21:21:48 +0900

    Fix MonsterLevel increasing level only one time

--- src/main/java/kro/dodoworld/advancedmonsters/modifiers/level/MonsterLevel.java
+++ src/main/java/kro/dodoworld/advancedmonsters/modifiers/level/MonsterLevel.java
@@ -7,28 +7,27 @@ import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class MonsterLevel {
-    private final FileConfiguration config;
 
     public MonsterLevel(){
-        config = MonsterEquipmentLevel.getMonsterEquipmentLevel();
+
     }
 
     public double getMonsterEquipmentLevel(World world){
-        return config.getDouble(world.getName());
-    }
-
-    public FileConfiguration getConfig() {
-        return config;
+        return MonsterEquipmentLevel.getMonsterEquipmentLevel().getDouble(world.getName());
     }
 
     public void increase(World world, double amount){
         WorldMonsterLevelIncreaseEvent event = new WorldMonsterLevelIncreaseEvent(world, amount);
         Bukkit.getServer().getPluginManager().callEvent(event);
         if(event.isCancelled()) return;
-        config.set(event.getWorld().getName(), config.getDouble(event.getWorld().getName()) + event.getAmount());
+        MonsterEquipmentLevel.getMonsterEquipmentLevel().set(event.getWorld().getName(), getMonsterEquipmentLevel(world) + event.getAmount());
+        MonsterEquipmentLevel.saveConfig();
+        MonsterEquipmentLevel.reloadConfig();
     }
 
     public void decrease(World world, double amount){
-        config.set(world.getName(), config.getDouble(world.getName()) - amount);
+        MonsterEquipmentLevel.getMonsterEquipmentLevel().set(world.getName(), MonsterEquipmentLevel.getMonsterEquipmentLevel().getDouble(world.getName()) - amount);
+        MonsterEquipmentLevel.saveConfig();
+        MonsterEquipmentLevel.reloadConfig();
     }
 }
