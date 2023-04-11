175a172df4acd751d57df5e958f384fda80c1e56
dodoman8067 <dodoman8067@gmail.com>
2023-04-11 21:22:07 +0900

    Change increase chance

--- src/main/java/kro/dodoworld/advancedmonsters/modifiers/level/increase/MonsterLevelIncrease.java
+++ src/main/java/kro/dodoworld/advancedmonsters/modifiers/level/increase/MonsterLevelIncrease.java
@@ -4,22 +4,23 @@ import io.papermc.paper.event.player.PlayerDeepSleepEvent;
 import kro.dodoworld.advancedmonsters.AdvancedMonsters;
 import org.bukkit.Raid;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.SpawnCategory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerFishEvent;
-import org.bukkit.event.raid.RaidTriggerEvent;
+import org.bukkit.event.raid.RaidStopEvent;
 
 public class MonsterLevelIncrease implements Listener {
     @EventHandler
     public void onKill(EntityDeathEvent event){
         if(event.getEntity().getKiller() == null) return;
         if(!event.getEntity().getSpawnCategory().equals(SpawnCategory.MONSTER)) return;
-        if(!event.getEntity().getKiller().getType().equals(EntityType.PLAYER)) return;
-        if((Math.random() * 100) <= 1){
-            AdvancedMonsters.getMonsterLevel().increase(event.getEntity().getWorld(), (Math.random() * 1.8));
+        if(!(event.getEntity().getKiller() instanceof Player)) return;
+        if((Math.random() * 100) <= 4) {
+            AdvancedMonsters.getMonsterLevel().increase(event.getEntity().getWorld(), 1);
         }
     }
 
@@ -42,16 +43,16 @@ public class MonsterLevelIncrease implements Listener {
     public void onBreed(EntityBreedEvent event){
         if(event.getBreeder() == null) return;
         if(!event.getBreeder().getType().equals(EntityType.PLAYER)) return;
-        if((Math.random() * 100) <= 0.8){
+        if((Math.random() * 100) <= 0.6){
             AdvancedMonsters.getMonsterLevel().increase(event.getBreeder().getWorld(), (Math.random() * 2));
         }
     }
 
     @EventHandler
-    public void onRaid(RaidTriggerEvent event){
+    public void onRaid(RaidStopEvent event){
         if(!event.getRaid().getStatus().equals(Raid.RaidStatus.LOSS)) return;
-        if((Math.random() * 100) <= 20){
-            AdvancedMonsters.getMonsterLevel().increase(event.getPlayer().getWorld(), (Math.random() * 6));
+        if((Math.random() * 100) <= 80){
+            AdvancedMonsters.getMonsterLevel().increase(event.getWorld(), (Math.random() * 6));
         }
     }
 }
