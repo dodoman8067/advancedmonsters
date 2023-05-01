55bf295781d18002d647c3e4f7e9465ee8ac7446
dodoman8067 <dodoman8067@gmail.com>
2023-05-01 22:48:02 +0900

    Rewrite TeleporterModifier.java

--- src/main/java/kro/dodoworld/advancedmonsters/modifier/ability/type/TeleporterModifier.java
+++ src/main/java/kro/dodoworld/advancedmonsters/modifier/ability/type/TeleporterModifier.java
@@ -10,33 +10,52 @@ import org.bukkit.entity.Monster;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TeleporterModifier {
+
+    private static final Set<Monster> teleportingMonsters = new HashSet<>();
 
 
     public static void run(AdvancedMonsters plugin) {
-        new BukkitRunnable(){
+        FileConfiguration config = TeleportModifierConfig.getTeleporterModifierConfig();
+        double range = config.getDouble("teleport_range");
 
+        new BukkitRunnable() {
             @Override
             public void run() {
-                for(World world : Bukkit.getWorlds()){
-                    for(LivingEntity entity : world.getLivingEntities()){
-                        if(entity instanceof Monster monster) {
-                            if(monster.getScoreboardTags().contains("adm_modifier_teleporter")){
-                                FileConfiguration config = TeleportModifierConfig.getTeleporterModifierConfig();
-                                double range = config.getDouble("teleport_range");
-                                if(monster.isDead()) cancel();
-                                if(monster.getTarget() != null){
-                                    if(!monster.getNearbyEntities(range, range, range).contains(monster.getTarget())){
-                                        monster.teleport(monster.getTarget().getLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN);
-                                    }
-                                }
+                for (World world : Bukkit.getWorlds()) {
+                    for (LivingEntity entity : world.getLivingEntities()) {
+                        if (!(entity instanceof Monster monster)) {
+                            continue;
+                        }
+                        if (!teleportingMonsters.contains(monster)) {
+                            continue;
+                        }
+                        if (monster.isDead()) {
+                            teleportingMonsters.remove(monster);
+                            continue;
+                        }
+                        if (monster.getTarget() != null) {
+                            if (monster.getLocation().distance(monster.getTarget().getLocation()) > range) {
+                                monster.teleport(monster.getTarget().getLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN);
                             }
                         }
                     }
                 }
             }
         }.runTaskTimer(plugin, 0L, 2L);
+
+        for (World world : Bukkit.getWorlds()) {
+            for (LivingEntity entity : world.getLivingEntities()) {
+                if (!(entity instanceof Monster monster)) {
+                    continue;
+                }
+                if (monster.getScoreboardTags().contains("adm_modifier_teleporter")) {
+                    teleportingMonsters.add(monster);
+                }
+            }
+        }
     }
-
-
 }
