8356c02a69157d4e2170e958993d3b2559f82c3f
dodoman8067 <dodoman8067@gmail.com>
2023-07-08 10:55:45 +0900

    Add some monster level increase cases

--- src/main/java/kro/dodoworld/advancedmonsters/modifier/level/increase/MonsterLevelIncrease.java
+++ src/main/java/kro/dodoworld/advancedmonsters/modifier/level/increase/MonsterLevelIncrease.java
@@ -2,7 +2,9 @@ package kro.dodoworld.advancedmonsters.modifier.level.increase;
 
 import io.papermc.paper.event.player.PlayerDeepSleepEvent;
 import kro.dodoworld.advancedmonsters.AdvancedMonsters;
+import org.bukkit.Material;
 import org.bukkit.Raid;
+import org.bukkit.entity.Boss;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.SpawnCategory;
@@ -11,13 +13,15 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerFishEvent;
-import org.bukkit.event.raid.RaidStopEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.raid.RaidFinishEvent;
 
 public class MonsterLevelIncrease implements Listener {
     @EventHandler
     public void onKill(EntityDeathEvent event){
         if(event.getEntity().getKiller() == null) return;
         if(!event.getEntity().getSpawnCategory().equals(SpawnCategory.MONSTER)) return;
+        if(event.getEntity() instanceof Boss) return;
         if(!(event.getEntity().getKiller() instanceof Player)) return;
         if((Math.random() * 100) <= 6) {
             AdvancedMonsters.getMonsterLevel().increase(event.getEntity().getWorld(), (Math.random() * 1.8));
@@ -26,8 +30,8 @@ public class MonsterLevelIncrease implements Listener {
 
     @EventHandler
     public void onSleep(PlayerDeepSleepEvent event){
-        if((Math.random() * 100) <= 3){
-            AdvancedMonsters.getMonsterLevel().increase(event.getPlayer().getWorld(), (Math.random() * 3));
+        if((Math.random() * 100) <= 4){
+            AdvancedMonsters.getMonsterLevel().increase(event.getPlayer().getWorld(), (Math.random() * 5));
         }
     }
 
@@ -36,6 +40,15 @@ public class MonsterLevelIncrease implements Listener {
         if(!event.getState().equals(PlayerFishEvent.State.CAUGHT_FISH)) return;
         if((Math.random() * 100) <= 2){
             AdvancedMonsters.getMonsterLevel().increase(event.getPlayer().getWorld(), (Math.random() * 1.4));
+        }
+    }
+
+    @EventHandler
+    public void onEat(PlayerItemConsumeEvent event){
+        if(!event.getItem().getType().isEdible()) return;
+        if(!isMeat(event.getItem().getType())) return;
+        if((Math.random() * 100) <= 0.2){
+            AdvancedMonsters.getMonsterLevel().increase(event.getPlayer().getWorld(), (Math.random() * 0.5));
         }
     }
 
@@ -49,10 +62,20 @@ public class MonsterLevelIncrease implements Listener {
     }
 
     @EventHandler
-    public void onRaid(RaidStopEvent event){
+    public void onRaid(RaidFinishEvent event){
         if(!event.getRaid().getStatus().equals(Raid.RaidStatus.LOSS)) return;
         if((Math.random() * 100) <= 80){
             AdvancedMonsters.getMonsterLevel().increase(event.getWorld(), (Math.random() * 6));
         }
+    }
+
+    private boolean isMeat(Material material){
+        if(!material.isEdible()) return false;
+        switch (material){
+            case BEEF, COOKED_BEEF, PORKCHOP, COOKED_PORKCHOP, CHICKEN, COOKED_CHICKEN, MUTTON, COOKED_MUTTON, RABBIT, COOKED_RABBIT, COD, COOKED_COD, SALMON, COOKED_SALMON -> {
+                return true;
+            }
+        }
+        return false;
     }
 }
