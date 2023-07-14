e79be012b2fc129530c582db93de23d764a9bae0
dodoman8067 <dodoman8067@gmail.com>
2023-07-14 17:48:42 +0900

    Add Ability#onSpawn

--- src/main/java/kro/dodoworld/advancedmonsters/modifier/ability/Ability.java
+++ src/main/java/kro/dodoworld/advancedmonsters/modifier/ability/Ability.java
@@ -1,9 +1,15 @@
 package kro.dodoworld.advancedmonsters.modifier.ability;
 
+import kro.dodoworld.advancedmonsters.AdvancedMonsters;
 import kro.dodoworld.advancedmonsters.core.registry.Registry;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import org.apache.commons.lang.WordUtils;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Monster;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -17,21 +23,24 @@ public abstract class Ability {
     private final Component name;
     protected final FileConfiguration abilityConfig;
     private final BukkitRunnable runnable;
+    private final TextColor displayColor;
 
     /**
      * Constructor for this class.
-     * @param id id for ability
-     * @param symbol symbol for ability. if null, the symbol will not appear on the monster's name
-     * @param name ability's user-friendly name
+     * @param id            id for ability
+     * @param symbol        symbol for ability. if null, the symbol will not appear on the monster's name
+     * @param name          ability's user-friendly name
      * @param abilityConfig configuration for the ability
-     * @param runnable scheduler for the ability. starts when it's registered
+     * @param runnable      scheduler for the ability. starts when it's registered
+     * @param displayColor color used on monster's name
      */
-    public Ability(@NotNull NamespacedKey id, @Nullable Component symbol, @NotNull Component name, @Nullable FileConfiguration abilityConfig, @Nullable BukkitRunnable runnable) {
+    public Ability(@NotNull NamespacedKey id, @Nullable Component symbol, @NotNull Component name, @Nullable FileConfiguration abilityConfig, @Nullable BukkitRunnable runnable, @Nullable TextColor displayColor) {
         this.id = id;
         this.runnable = runnable;
         this.symbol = symbol;
         this.name = name;
         this.abilityConfig = abilityConfig;
+        this.displayColor = displayColor;
     }
 
     /**
@@ -46,7 +55,19 @@ public abstract class Ability {
     /**
      * Called when registered.
      */
-    public void init(){}
+    public abstract void init();
+
+    /**
+     * Called when monster spawned with this ability.
+     * @param monster the monster spawned with this ability
+     */
+    public void onSpawn(Monster monster){
+        monster.getPersistentDataContainer().set(new NamespacedKey(AdvancedMonsters.getPlugin(AdvancedMonsters.class), "ability"), PersistentDataType.STRING, this.id.asString());
+        monster.setCustomNameVisible(true);
+        if(this.symbol != null){
+            monster.customName(this.symbol.append(this.name).append(Component.text(" ").append(toMobName(monster))));
+        }
+    }
 
     /**
      * Returns the config.
@@ -88,10 +109,24 @@ public abstract class Ability {
 
     /**
      * Returns ability's scheduler.
-     * @return {@link BukkitRunnable} instance, null if the ability didn't have scheduler initialized.
+     * @return {@link BukkitRunnable} instance, null if the ability didn't have a scheduler initialized.
      */
     @Nullable
     public BukkitRunnable getRunnable() {
         return runnable;
+    }
+
+    /**
+     * Returns monster name color.
+     * @return gray if ability was initialized as null, otherwise color initialized by constructor
+     */
+    @NotNull
+    public TextColor getDisplayColor() {
+        if(displayColor == null) return NamedTextColor.GRAY;
+        return displayColor;
+    }
+
+    private Component toMobName(Monster monster){
+        return Component.text(WordUtils.capitalize(monster.getType().name().toUpperCase()), this.getDisplayColor());
     }
 }
--- src/main/java/kro/dodoworld/advancedmonsters/modifier/apply/ModifierApplier.java
+++ src/main/java/kro/dodoworld/advancedmonsters/modifier/apply/ModifierApplier.java
@@ -1,4 +1,34 @@
 package kro.dodoworld.advancedmonsters.modifier.apply;
 
-public class ModifierApplier {
+import kro.dodoworld.advancedmonsters.AdvancedMonsters;
+import kro.dodoworld.advancedmonsters.core.registry.Registry;
+import kro.dodoworld.advancedmonsters.modifier.ability.Ability;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Monster;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ModifierApplier implements Listener {
+    @EventHandler
+    public void onSpawn(CreatureSpawnEvent event){
+        if(event.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.CUSTOM)) return;
+
+    }
+
+    private void applyAbility(Monster monster, Ability ability){
+        if(!ability.isRegistered()) AdvancedMonsters.getPlugin(AdvancedMonsters.class).getLogger().severe("Plugin tried to add unknown / unregistered ability : " + ability.getId().asString());
+        ability.onSpawn(monster);
+    }
+
+    private Ability getRandomAbility(){
+        List<Ability> abilitySet = new ArrayList<>(Registry.getRegisteredAbilities());
+        Collections.shuffle(abilitySet);
+        return abilitySet.get(0);
+    }
 }
