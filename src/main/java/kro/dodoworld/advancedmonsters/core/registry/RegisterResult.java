35ddbe9be9fa78ca58db575d35cf3aa23ad62354
dodoman8067 <dodoman8067@gmail.com>
2023-07-24 23:28:52 +0900

    Document RegisterResult.java

--- src/main/java/kro/dodoworld/advancedmonsters/core/registry/RegisterResult.java
+++ src/main/java/kro/dodoworld/advancedmonsters/core/registry/RegisterResult.java
@@ -1,7 +1,19 @@
 package kro.dodoworld.advancedmonsters.core.registry;
 
+/**
+ * Represents register result for registrable class.
+ */
 public enum RegisterResult {
+    /**
+     * Represents successful register result.
+     */
     SUCCESS,
+    /**
+     * Represents failure register result.
+     */
     FAIL,
+    /**
+     * Represents an unknown register result.
+     */
     UNKNOWN
 }
