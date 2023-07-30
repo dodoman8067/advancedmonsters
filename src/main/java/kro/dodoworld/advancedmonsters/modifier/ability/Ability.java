1f6dbc1084944ce829840f5dd4df5530e6aa2ef4
dodoman8067 <dodoman8067@gmail.com>
2023-07-30 20:03:34 +0900

    Remove unused imports

--- src/main/java/kro/dodoworld/advancedmonsters/modifier/ability/Ability.java
+++ src/main/java/kro/dodoworld/advancedmonsters/modifier/ability/Ability.java
@@ -2,7 +2,6 @@ package kro.dodoworld.advancedmonsters.modifier.ability;
 
 import kro.dodoworld.advancedmonsters.AdvancedMonsters;
 import kro.dodoworld.advancedmonsters.core.registry.Registrable;
-import kro.dodoworld.advancedmonsters.core.registry.Registry;
 import kro.dodoworld.advancedmonsters.util.AbilityUtils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
