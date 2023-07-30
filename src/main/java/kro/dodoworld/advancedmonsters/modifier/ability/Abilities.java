838a4678256bd8c27c0e4d073e2c482b0eb21480
dodoman8067 <dodoman8067@gmail.com>
2023-07-30 14:59:41 +0900

    Abilities.java is now final

--- src/main/java/kro/dodoworld/advancedmonsters/modifier/ability/Abilities.java
+++ src/main/java/kro/dodoworld/advancedmonsters/modifier/ability/Abilities.java
@@ -21,7 +21,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Abilities implements Listener {
+public final class Abilities implements Listener {
 
     private static Ability healthy = null;
     private static Ability strong = null;
