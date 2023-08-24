14012533d05f338e0fd28fad7422577d4c642038
dodoman8067 <dodoman8067@gmail.com>
2023-08-24 20:49:50 +0900

    RegistryInitializeEvent will be called immediately after calling Registry#init method

--- src/main/java/kro/dodoworld/advancedmonsters/core/registry/Registry.java
+++ src/main/java/kro/dodoworld/advancedmonsters/core/registry/Registry.java
@@ -4,9 +4,6 @@ import kro.dodoworld.advancedmonsters.AdvancedMonsters;
 import kro.dodoworld.advancedmonsters.event.registry.RegistryInitializeEvent;
 import kro.dodoworld.advancedmonsters.event.registry.RegistryRegisterObjectEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.Plugin;
 
 import java.util.Collections;
@@ -16,7 +13,7 @@ import java.util.Set;
 /**
  * Class for handling the registry system
  */
-public final class Registry implements Listener {
+public final class Registry {
 
     private Registry(){
     }
@@ -38,7 +35,7 @@ public final class Registry implements Listener {
         if(!(plugin instanceof AdvancedMonsters)) throw new RuntimeException(new IllegalAccessException("Registry cannot be initialized on other plugins."));
         if(isInitialized) throw new RuntimeException(new IllegalAccessException("Registry has been already initialized."));
         isInitialized = true;
-        Bukkit.getServer().getPluginManager().registerEvents(INSTANCE, AdvancedMonsters.getPlugin(AdvancedMonsters.class));
+        Bukkit.getServer().getPluginManager().callEvent(new RegistryInitializeEvent(INSTANCE));
     }
 
     /**
@@ -46,16 +43,10 @@ public final class Registry implements Listener {
      * @param registrable the registrable object
      */
     public void register(Registrable registrable){
-        if(registrable.isRegistered()) throw new IllegalArgumentException("You cannot register an object with id that already exists");
-        if(registrable.init().equals(RegisterResult.FAIL)) throw new IllegalStateException("Registration failed");
+        if(registrable.isRegistered()) throw new RuntimeException(new IllegalArgumentException("You cannot register an object with id that already exists"));
+        if(registrable.init().equals(RegisterResult.FAIL)) throw new RuntimeException(new IllegalStateException("Registration failed"));
         RegistryRegisterObjectEvent event = new RegistryRegisterObjectEvent(registrable);
         Bukkit.getPluginManager().callEvent(event);
         REGISTERED_OBJECTS.add(registrable);
-    }
-
-    @EventHandler
-    private void onEnable(PluginEnableEvent event){
-        if(!(event.getPlugin() instanceof AdvancedMonsters)) return;
-        Bukkit.getServer().getPluginManager().callEvent(new RegistryInitializeEvent(INSTANCE));
     }
 }
