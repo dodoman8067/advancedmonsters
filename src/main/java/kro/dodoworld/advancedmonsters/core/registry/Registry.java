03aaa72f4dd4da0040406f16d11018960aec72bf
dodoman8067 <dodoman8067@gmail.com>
2023-08-24 21:16:25 +0900

    Add Registry#unRegister

--- src/main/java/kro/dodoworld/advancedmonsters/core/registry/Registry.java
+++ src/main/java/kro/dodoworld/advancedmonsters/core/registry/Registry.java
@@ -3,6 +3,7 @@ package kro.dodoworld.advancedmonsters.core.registry;
 import kro.dodoworld.advancedmonsters.AdvancedMonsters;
 import kro.dodoworld.advancedmonsters.event.registry.RegistryInitializeEvent;
 import kro.dodoworld.advancedmonsters.event.registry.RegistryRegisterObjectEvent;
+import kro.dodoworld.advancedmonsters.event.registry.RegistryUnRegisterObjectEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
@@ -41,6 +42,7 @@ public final class Registry {
     /**
      * Registers an object that implements {@link Registrable}.
      * @param registrable the registrable object
+     * @see Registry#unRegister(Registrable registrable)
      */
     public void register(Registrable registrable){
         if(registrable.isRegistered()) throw new RuntimeException(new IllegalArgumentException("You cannot register an object with id that already exists"));
@@ -48,5 +50,18 @@ public final class Registry {
         RegistryRegisterObjectEvent event = new RegistryRegisterObjectEvent(registrable);
         Bukkit.getPluginManager().callEvent(event);
         REGISTERED_OBJECTS.add(registrable);
+    }
+
+    /**
+     * Unregisters an object that implements {@link Registrable}.
+     * This method is intended for addons to remove an ability that has been registered by other addons/main plugin.
+     * @param registrable the registrable object
+     * @see Registry#register(Registrable registrable)
+     */
+    public void unRegister(Registrable registrable){
+        if(!registrable.isRegistered()) throw new RuntimeException(new IllegalArgumentException("You cannot unregister an object with id that doesn't exists"));
+        RegistryUnRegisterObjectEvent event = new RegistryUnRegisterObjectEvent(registrable);
+        Bukkit.getPluginManager().callEvent(event);
+        REGISTERED_OBJECTS.remove(registrable);
     }
 }
