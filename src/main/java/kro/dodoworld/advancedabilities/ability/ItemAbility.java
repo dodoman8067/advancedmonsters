0f4fb3df0d4252be9d66b02cb52e9c9875197c1e
dodoman8067 <dodoman8067@gmail.com>
2024-09-11 17:30:54 +0900

    Remove onHit

--- src/main/java/kro/dodoworld/advancedabilities/ability/ItemAbility.java
+++ src/main/java/kro/dodoworld/advancedabilities/ability/ItemAbility.java
@@ -3,9 +3,7 @@ package kro.dodoworld.advancedabilities.ability;
 import kro.dodoworld.advancedmonsters.core.registry.Registrable;
 import net.kyori.adventure.text.Component;
 import org.bukkit.NamespacedKey;
-import org.bukkit.entity.Entity;
 import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
@@ -33,8 +31,6 @@ public abstract class ItemAbility implements Registrable {
         }
         return false;
     }
-
-    public abstract void onHit(Entity victim, Entity attacker, ItemStack appliedItem, int tier);
 
     public abstract boolean canBeAppliedAt(EquipmentSlot slot, int tier);
 
